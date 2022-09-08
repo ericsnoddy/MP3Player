7736019f771da39336d68715799a081ba9f121c9
ericsnoddy <ericsnoddy@gmail.com>
2022-09-08 19:18:14 -0400

    splice and generate song titles

--- obj/display.py
+++ obj/display.py
@@ -11,33 +11,30 @@ class ListUI:
         self.border = self.rect.copy()
 
         self.song_paths = song_paths
-        self.song_names = self._generate_titles(song_paths)
-        print(self.song_names)
+        self.song_titles = self._get_titles_list(self.song_paths)
         self.now_playing_index = now_playing_index
-        self.now_playing = self._splice_title(song_paths[now_playing_index])
+        self.now_playing = self.song_titles[self.now_playing_index]
 
         # fonts
         self.font_list = pg.font.Font(FONT_TYPE_REG, LIST_FONTSIZE_REG )
         self.font_list_bold = pg.font.Font(FONT_TYPE_BOLD, LIST_FONTSIZE_BOLD)
 
-    def _generate_titles(self, song_paths):
-        song_names = []
+    def _get_titles_list(self, song_paths):
+        song_titles = []
         filenames = [song_path.rsplit('\\') for song_path in song_paths]
         for list in filenames:
             # of the split names we want the last indexed value; then split off the filetype and keep the 0 index value
-            song_names.append(list[-1].rsplit('.')[0])
-        return song_names
+            # Due to filtering in the setup process all files will have a proper extension
+            song_titles.append(list[-1].rsplit('.')[0])
+        return song_titles
 
-    def _splice_title(self, song_path):
-        # ex path = 'C:/Users/Eric/Music\Alanis Morissette\Jagged Little Pill [25th Anniversary Deluxe Edition]\01 - All I Really Want.mp3'
-        song = song_path.rsplit('\\')        
-        return song[-1].rsplit('.')[0]
-
-    def generate_list(self):
-        pass
+    def _generate_titles(self):
+        titles = [name for name in self.song_titles]
+        for title in titles:
+            yield title
 
     def update(self, new_index):
-        self.now_playing = self._splice_title(self.song_paths[new_index])
+        self.now_playing = self.song_titles[new_index]
 
     def draw(self):
         # pg.draw.rect(self.win, 'black', self.rect)
