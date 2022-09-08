3c7f0adc3bdaffe7cd0f423d53c6a7b704d5d5a0
ericsnoddy <ericsnoddy@gmail.com>
2022-09-08 18:51:27 -0400

    preparing to add scrollbar to console

--- main.py
+++ main.py
@@ -2,27 +2,17 @@ import pygame as pg
 import sys
 from pygame.locals import *
 
-from obj.settings import WIDTH, HEIGHT, FPS, CAPTION
+from obj.settings import WIDTH, HEIGHT, FPS
 from obj.console import Console
-
-def init_pygame():
-    
-    # initialize all pg modules
-    pg.init()
-
-    WIN = pg.display.set_mode((WIDTH, HEIGHT))
-    pg.display.set_caption(CAPTION)
-    clock = pg.time.Clock()
-
-    return WIN, clock
 
 def main():
     
-    WIN, clock = init_pygame()
-    console = Console(WIN)
+    # init
+    win, clock = init_pygame()
+    console = Console(win)
 
-    running = True
     # program loop
+    running = True
     while running:
 
         console.run()
@@ -34,7 +24,10 @@ def main():
             
             event_handler(console, event)        
         
+        # update the display
         pg.display.update()
+
+        # tick the clock to maintain FPS
         clock.tick(FPS)
 
     pg.quit()
@@ -44,30 +37,27 @@ def event_handler(console, event):
 
     if event.type == MOUSEBUTTONDOWN:
 
+        # get clicked sprite (by filtering all sprites for click event - will return only single sprite)
         clicked = [btn for btn in console.buttons.sprites() if btn.is_clicked(event.pos)]
+
         for btn in clicked:
             if btn.label == 'power':
                 console.power_down()
 
             if btn.label == 'mute' and btn.can_click:
-                btn.log_click()
                 console.mute(btn)
 
             if btn.label == 'stop' and btn.can_click:
-                btn.log_click()
                 console.stop(btn)
 
             if btn.label == 'play' and btn.can_click:
-                btn.log_click()
                 console.play(btn)
 
             if btn.label == 'pause' and btn.can_click:
-                btn.log_click()
                 console.pause(btn)
 
             if btn.label == 'prev' or btn.label == 'next':
                 if btn.can_click:
-                    btn.log_click()
                     console.skip(btn)
 
             if btn.label[0:3] == 'vol':                
@@ -75,18 +65,40 @@ def event_handler(console, event):
 
             if btn.label == 'rew' or btn.label == 'ff':
                 if btn.can_click:
-                    btn.log_click()
                     console.seek(btn)
+        
+        if console.volbar.is_clicked(event.pos):
+            console.adjust_volbar(event.pos[0])
+            
+        elif console.progbar.is_clicked(event.pos):
+            console.adjust_progbar(event.pos[0])
 
     if event.type == MOUSEBUTTONUP:
 
-        vols = [btn for btn in console.buttons.sprites() if btn.label[0:3] == 'vol']        
+        # Check if volup or down is active; deactivate if so
+        vols = [btn for btn in console.buttons.sprites() if btn.label[0:3] == 'vol']
+
         for vol in vols:
             if vol.is_active:
                 vol.activate(False)
 
-    if event.type == pg.event.Event(console.SONG_OVER):
+    # catch custom flag
+    if event.type == console.SONG_OVER:
         console.song_over()
+
+def init_pygame():
+    
+    # initialize all pg modules
+    pg.init()
+
+    # init display module and caption
+    win = pg.display.set_mode((WIDTH, HEIGHT))
+    pg.display.set_caption('Thinamp')
+
+    # init clock module for capping the framerate
+    clock = pg.time.Clock()
+
+    return win, clock
 
 if __name__ == '__main__':
     main()
