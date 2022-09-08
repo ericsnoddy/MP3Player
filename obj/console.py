3f74a5b7553e976ee69a059f32d470041ada76b8
ericsnoddy <ericsnoddy@gmail.com>
2022-09-08 18:51:51 -0400

    progbar fully functional

--- obj/console.py
+++ obj/console.py
@@ -1,28 +1,33 @@
-from os import walk
-from os.path import join
-from random import randrange
-from socket import SO_LINGER
-import sys
-
 import pygame as pg
 from pygame.locals import *
 from tkinter import filedialog
-from mutagen.oggvorbis import OggVorbis
+    # metadata extraction
+from mutagen.oggvorbis import OggVorbis as Ogg
+from mutagen.mp3 import MP3
 
-from obj.button import ToggleButton, QuickButton, HoldButton, MuteButton, StopButton, SeekButton
+from os import walk
+from os.path import join
+from random import randrange
+
 from obj.slider import Slider
+from obj.display import ListUI
+from obj.button import ToggleButton, QuickButton, HoldButton, MuteButton, StopButton, SeekButton
+from obj.data import BG_DEFAULT, FONT_TYPE_REG, FONT_TYPE_BOLD
 from obj.settings import (
     WIDTH, 
     HEIGHT, 
-    PAD, BPAD, 
-    BSIZE, 
-    BG_DEFAULT, 
-    FONT_TYPE_REG, 
-    FONT_TYPE_BOLD, 
+    PAD, 
+    BPAD, 
+    BSIZE,
+    ROWPAD,
+    ROWSLOT,
+    ROW1_Y,
+    ROW2_Y,
+    ROW3_Y,
+    UI_HEIGHT,
     FONT_COLOR, 
-    VOL_START, 
-    VOL_MAX, 
-    # VOL_INCR, 
+    VOL_START,
+    SEEK_INCR,
     OPEN_RANDOM_MODE,
 )
 
@@ -31,93 +36,111 @@ from obj.debug import debug
 class Console:
     def __init__(self, win):
 
-        self.debug = True
-        self.font_debug = pg.font.Font(FONT_TYPE_REG, 6)
-
         self.running = True
         self.win = win
 
         # audio
         self.volume = VOL_START
-        pg.mixer.music.set_volume(self.volume / VOL_MAX)
+        pg.mixer.music.set_volume(self.volume / 100)
 
+        # background
         self.bg = pg.image.load(BG_DEFAULT).convert()
+
+        # fonts
         self.font_reg = pg.font.Font(FONT_TYPE_REG, 14)
+        self.font_duration = pg.font.Font(FONT_TYPE_BOLD, 30)
         self.font_goodbye = pg.font.Font(FONT_TYPE_BOLD, 50)
-        self.font_list = pg.font.Font(FONT_TYPE_REG, 8)
-        self.font_list_bold = pg.font.Font(FONT_TYPE_BOLD, 10)
 
         # default flags and user setup
         self.setup_mode = True
         self.music_folder = None
-        self.collection = []
         self.song_paths = []
-        # self.collection = ['Boss Fight.ogg', "Child's Nightmare.ogg"]
-        # self.song_paths = ['C:/Users/Eric/Workspace/test_music/Dark\\Boss Fight.ogg', "C:/Users/Eric/Workspace/test_music/Dark\\Child's Nightmare.ogg"]
 
-        # player flags
+        # console player flags
         self.song_in_progress = False
         self.now_playing_index = 0
         self.song_offset = 0
         self.song_length = 0
 
-        # for setting event for end of song
+        # custom flag for event handler - song has ended its duration
         self.SONG_OVER = pg.USEREVENT+1
+        pg.mixer.music.set_endevent(self.SONG_OVER)
 
-        # All the text that can be pre-rendered
-        self.setup_txt = self.font_reg.render('Select music folder', True, FONT_COLOR)
+        # All the text that can be pre-rendered for performance
+        self.setup_txt = self.font_reg.render('Browse to music folders', True, FONT_COLOR)
+        self.setup_txt2 = self.font_reg.render('supports .ogg and .mp3', True, FONT_COLOR)
         self.goodbye_txt = self.font_goodbye.render('Ciao!', True, FONT_COLOR)
 
-        # buttons (requires self.setup_txt)
+        # button sprite group & slider bar setup
         self.buttons = pg.sprite.Group()
         self.setup_button = pg.sprite.GroupSingle()
-        self._group_buttons()
+        self._group_btns()
+        self._init_bars()
+
+        # see setup for file display ui init
 
     def setup(self):
         if not self.music_folder:
+            # display setup text and button
             self.win.blit(self.setup_txt, ((WIDTH - PAD*2 - self.setup_txt.get_width()) // 2 + PAD, PAD))
             self.setup_button.draw(self.win)
+            self.win.blit(self.setup_txt2, ((WIDTH - PAD*2 - self.setup_txt2.get_width()) // 2 + PAD, PAD + self.setup_txt.get_height() + PAD + BSIZE + PAD))
 
+            # get folder if clicked
             setup_btn = self.setup_button.sprite
             clicks = pg.mouse.get_pressed()            
             if clicks[0] and setup_btn.is_clicked(pg.mouse.get_pos()):
                 self.music_folder = filedialog.askdirectory( title='Select music folder' )
 
-        elif not self.collection:
+        # do not proceed without proper filepath to music folder
+        elif not self.song_paths:
             for root, _, files in walk(self.music_folder):
-                for name in files:
+                for filename in files:
                     # Creates 2 lists that are index-synced: one for full filepath, 
-                    # and one for file names; so I don't have to deal with slicing later
-                    if name.endswith('.ogg') or name.endswith('.mp3'):                    
-                        self.song_paths.append(join(root, name))
-                        self.collection.append(name)
-            # no valid filetypes - start again
-            if len(self.collection) == 0:
+                    # and one for file filenames; so I don't have to deal with slicing later
+                    if filename.endswith('.ogg') or filename.endswith('.mp3'):                    
+                        self.song_paths.append(join(root, filename))
+
+            # no valid filetypes - start over
+            if len(self.song_paths) == 0:
                 self.music_folder = None
         else:
+            # init file display ui - 349px height exactly fits 28 rows of 10pt font + 1 row 11pt font
+            self.list_ui = ListUI(self.win, PAD//2, 2*PAD - 2 , WIDTH - PAD, UI_HEIGHT, self.song_paths, self.now_playing_index)   
+
+            # load first track and end setup mode
             if OPEN_RANDOM_MODE:
-                self.now_playing_index = randrange(0, len(self.song_paths))
-            # load first track:            
+                self.now_playing_index = randrange(0, len(self.song_paths))       
             pg.mixer.music.load(self.song_paths[self.now_playing_index])
             self._log_song_length()
-            self.setup_mode = False  
+            self.setup_mode = False
 
     def mute(self, mute_btn):
+        mute_btn.log_click()
+
+        # toggle button, adjust volume bar, adjust volume
         self.volume = mute_btn.toggle_mute(self.volume)
-        pg.mixer.music.set_volume(self.volume)
+        self.volbar.value_to_pos(self.volume, 100)
+        pg.mixer.music.set_volume(self.volume / 100)
 
     def stop(self, stop_btn):
-        if self.song_in_progress:
-            stop_btn.activate()
+        stop_btn.log_click()
+        if self.song_in_progress:          
+
             self.song_in_progress = False
-            self.song_offset = 0
+
+            # deactivate/activate relevant btns
             play_btn = self._get_button('play')
             play_btn.activate(False)
+
             pause_btn = self._get_button('pause')
             pause_btn.activate(False)
+
+            stop_btn.activate()
             pg.mixer.music.stop()
 
     def play(self, play_btn):
+        play_btn.log_click()
         if self.song_in_progress and not pg.mixer.music.get_busy():
             pause_btn = self._get_button('pause')
             if pause_btn.is_active:
@@ -134,6 +157,7 @@ class Console:
             pg.mixer.music.play()
 
     def pause(self, pause_btn):
+        pause_btn.log_click()
         play_btn = self._get_button('play')
         if self.song_in_progress and pg.mixer.music.get_busy():
             pause_btn.activate()            
@@ -145,6 +169,7 @@ class Console:
             pg.mixer.music.unpause()
 
     def skip(self, skip_btn):
+        skip_btn.log_click()
         is_busy = pg.mixer.music.get_busy()
         if self.song_in_progress and is_busy:
             self._load_song(skip_btn.label)
@@ -157,27 +182,54 @@ class Console:
         else:
             self._load_song(skip_btn.label)
 
+    def seek(self, seek_btn, incr=SEEK_INCR):
+        seek_btn.log_click()
+        if seek_btn.label == 'rew':
+            self.song_offset = self._get_position(self.song_offset, rew=incr)            
+
+        if seek_btn.label == 'ff':
+            self.song_offset = self._get_position(self.song_offset, ff=incr)
+        pg.mixer.music.play(0, self.song_offset)
+
     def volumize(self, vol_btn):
         vol_btn.activate()
 
         mute_btn = self._get_button('mute')
         if mute_btn.is_active:
-            mute_btn.toggle_mute()
+            self.volume = mute_btn.toggle_mute(self.volume)   
 
-        if vol_btn.label == 'volup' and self.volume < VOL_MAX:
+        if vol_btn.label == 'volup' and self.volume < 100:
             self.volume += 1
-            pg.mixer.music.set_volume(self.volume / VOL_MAX)
         if vol_btn.label == 'voldown' and self.volume > 0:
             self.volume -= 1
-            pg.mixer.music.set_volume(self.volume / VOL_MAX)  
 
-    def seek(self, seek_btn):
-        if seek_btn.label == 'rew':
-            self.song_offset = self._get_position(self.song_offset, rew=10)            
+        self.volbar.value_to_pos(self.volume, 100)        
+        pg.mixer.music.set_volume(self.volume / 100)        
 
-        if seek_btn.label == 'ff':
-            self.song_offset = self._get_position(self.song_offset, ff=10)
-        pg.mixer.music.play(0, self.song_offset)
+    def adjust_volbar(self, click_x):
+        self.volume = self.volbar.pos_to_value(click_x, 100)
+        pg.mixer.music.set_volume(self.volume / 100)
+        self.volbar.value_to_pos(self.volume, 100)
+
+    def adjust_progbar(self, click_x):
+        # This returns the position mark in seconds we need to seek to
+        new_position = self.progbar.pos_to_value(click_x, self.song_length)
+        current_position = self._get_position(self.song_offset)
+        if new_position > current_position:
+            ff_btn = self._get_button('ff')
+            self.seek(ff_btn, incr=(new_position - current_position))
+        elif new_position < current_position:
+            rew_btn = self._get_button('rew')
+            self.seek(rew_btn, incr=(current_position - new_position))
+
+    def song_over(self):
+        # don't cycle if the song was stopped by the user
+        stop_btn = self._get_button('stop')
+        if not stop_btn.is_active:
+            self.song_in_progress = False
+            self._load_song('next')
+            play_btn = self._get_button('play')
+            self.play(play_btn)
 
     def power_down(self):
         self.win.blit(self.bg, (0,0))
@@ -191,27 +243,30 @@ class Console:
         pg.time.delay(3000)
         self.running = False
 
-    ##
     ## PRIVATE METHODS
     ##
-    def _song_over(self):
-        pass
+    def _get_formatted_duration(self):
 
-    def _get_duration(self):
-        if not self.song_in_progress:
-            position = self.song_length
-        else:
-            position = round(self._get_position(self.song_offset), 2)
+        position = round(self._get_position(self.song_offset), 2)
+
         hours = "{:0>2d}".format(int(position // 3600))
         mins = "{:0>2d}".format(int(position // 60))
         secs = "{:0>2d}".format(int(position % 60))
         tenths = int(10 * (position % 60 - int(position % 60)))
 
-        print(hours, mins, secs, tenths)
-        return hours, mins, secs, tenths
+        return f'{hours}:{mins}:{secs}.{tenths}'
+
+    def _display_duration(self):
+        time_played_text = self.font_duration.render(self._get_formatted_duration(), True, FONT_COLOR)
+        self.win.blit(time_played_text, ((WIDTH - PAD*2 - time_played_text.get_width()) // 2 + PAD, 10))
 
     def _get_position(self, start_pos, rew=0, ff=0):
-        position = pg.mixer.music.get_pos() / 1000 + start_pos - rew + ff
+
+        if not self.song_in_progress: pos = 0
+        else: pos = pg.mixer.music.get_pos()
+        
+        position = pos / 1000 + start_pos - rew + ff
+
         if position < 0:
             return 0
         elif position > self.song_length:
@@ -232,15 +287,22 @@ class Console:
             # do not restart the track
             selection = 'pass'
 
-        # custom end event to alert when song is over (non-user only)
-        pg.mixer.music.set_endevent(self.SONG_OVER)
         if selection != 'pass':
             self.song_offset = 0
-            pg.mixer.music.load(self.song_paths[self.now_playing_index])
-            self._log_song_length()      
+            try:    # make sure file is not corrupt/unreadable, else go to next song
+                pg.mixer.music.load(self.song_paths[self.now_playing_index])
+                self._log_song_length() 
+            except:
+                next_btn = self._get_button('next')
+                self.skip(next_btn)                 
 
     def _log_song_length(self):
-        song = OggVorbis(self.song_paths[self.now_playing_index])
+        song = self.song_paths[self.now_playing_index]
+        if song.endswith('.ogg'):
+            song = Ogg(song)
+        else:
+            song = MP3(song)
+
         self.song_length = song.info.length
 
     def _get_button(self, label):
@@ -250,37 +312,37 @@ class Console:
                 btns.append(btn)
         return btns[0]
 
-    def _group_buttons(self):
+    def _group_btns(self):
         # add setup icon to GroupSingle sprite group
         centeringx = (WIDTH - PAD*2 - BSIZE) // 2 + PAD
         centeringy = PAD + self.setup_txt.get_height() + PAD
-        self.setup_button.add(ToggleButton('menu', centeringx, centeringy))
+        self.setup_button.add(ToggleButton('menu', centeringx, centeringy))  
 
-        # Spacing for icons (sprites)
-        rowpad = (WIDTH - BSIZE*7 - BPAD*6) // 2
-        rowslot = BSIZE + BPAD
-        row1_y = HEIGHT - BPAD*3 - BSIZE*3  
-        row2_y = HEIGHT - BPAD*2 - BSIZE*2
-        row3_y = HEIGHT - BPAD - BSIZE        
+        # add buttons to sprite group - passed functions do not include ()
+        self.buttons.add(            
+            QuickButton('prev', ROWPAD, ROW1_Y),
+            HoldButton('voldown', ROWPAD + ROWSLOT, ROW2_Y, self.volumize),
+            StopButton('stop', ROWPAD + 2*ROWSLOT, ROW1_Y),
+            ToggleButton('play', ROWPAD + 3*ROWSLOT, ROW1_Y),
+            ToggleButton('pause', ROWPAD + 4*ROWSLOT, ROW1_Y),
+            SeekButton('ff', ROWPAD + 5*ROWSLOT, ROW1_Y),
+            QuickButton('next', ROWPAD + 6*ROWSLOT, ROW1_Y),
+            HoldButton('volup', WIDTH - ROWPAD - BSIZE - ROWSLOT, ROW2_Y, self.volumize),
+            SeekButton('rew', ROWPAD + ROWSLOT, ROW1_Y),             
+            MuteButton('mute', WIDTH - ROWPAD - BSIZE, ROW2_Y, self.volume),            
+            # Button('menu', ROWPAD, ROW2_Y),
+            ToggleButton('power', (WIDTH - BSIZE) // 2, ROW3_Y),  
+        )
 
-        # add buttons to sprite group
-        self.buttons.add(
-            
-            QuickButton('prev', rowpad, row1_y),
-            HoldButton('voldown', rowpad + rowslot, row2_y, self.volumize),
-            StopButton('stop', rowpad + 2*rowslot, row1_y),
-            ToggleButton('play', rowpad + 3*rowslot, row1_y),
-            ToggleButton('pause', rowpad + 4*rowslot, row1_y),
-            SeekButton('ff', rowpad + 5*rowslot, row1_y),
-            QuickButton('next', rowpad + 6*rowslot, row1_y),
-            HoldButton('volup', WIDTH - rowpad - BSIZE - rowslot, row2_y, self.volumize),
-            SeekButton('rew', rowpad + rowslot, row1_y),             
-            MuteButton('mute', WIDTH - rowpad - BSIZE, row2_y, self.volume),            
-            # Button('menu', rowpad, row2_y),
-            ToggleButton('power', (WIDTH - BSIZE) // 2, row3_y),  
-        )  
+    def _init_bars(self):
+        # volume bar
+        x = ROWPAD + 2*ROWSLOT
+        w = WIDTH - ROWPAD - BSIZE - ROWSLOT - BPAD - x
+        self.volbar = Slider(x, ROW2_Y + 10, w, BSIZE - 20, self.volume, 100)
 
-    ##
+        # song progress bar
+        self.progbar = Slider(BPAD, ROW1_Y - BSIZE + 2*BPAD, WIDTH - 2*BPAD, BPAD + 3, self._get_position(self.song_offset), self.song_length)
+
     ## RUN CONSOLE
     ##
     def run(self):
@@ -288,15 +350,19 @@ class Console:
 
         if self.running:
             if not self.setup_mode:
+
                 self.buttons.update()
                 self.buttons.draw(self.win)
 
-                # duration display
-                hours, mins, secs, tenths = self._get_duration()
-                time_played_text = self.font_goodbye.render(f'{hours}:{mins}:{secs}.{tenths}', True, FONT_COLOR)
-                self.win.blit(time_played_text, ((WIDTH - PAD*2 - time_played_text.get_width()) // 2 + PAD, PAD))
+                self._display_duration()
+
+                # clickable volume and progress bars
+                self.volbar.draw()
+                self.progbar.value_to_pos(self._get_position(self.song_offset), self.song_length)
+                self.progbar.draw()
+
+                # clickable songlist
+                self.list_ui.update(self.now_playing_index)
+                self.list_ui.draw()
             else: 
                 self.setup()
-
-        if debug:
-            debug([f'sip: {self.song_in_progress}, off: {self.song_offset}, pos: {pg.mixer.music.get_pos()}'])
