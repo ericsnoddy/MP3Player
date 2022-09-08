f4e57c2718154e254749ab076e9d73f76120685c
ericsnoddy <ericsnoddy@gmail.com>
2022-09-08 18:53:05 -0400

    trying to ignore debug.py, can't get it

--- obj/debug.py
+++ obj/debug.py
@@ -1,25 +1,25 @@
 import pygame
 pygame.init()
 
-# Attributes
-BG_COLOR = (0,0,0)
-FONT_COLOR = (255,255,255)
+# Constants
+BG_COLOR = 'black'
+FONT_COLOR = 'white'
 TOPLEFT = (10,10)
 FONT = None  # None for pygame default font
 ANTIALIAS = True
 
-# This method converts the input list to a string and displays it
-# as a rect at position topleft = (x,y), default included
-# Must input a list (single strings get chopped)
+# This method converts list of strings to a multi-line overlay,
+# with rect at position topleft = (x,y) = TOPLEFT
+# Must input as a list of strings even if single line
 
-def debug(info_list, x = TOPLEFT[0], y = TOPLEFT[1]):
+def debug(multistring_list, x=TOPLEFT[0], y=TOPLEFT[1]):
     f = pygame.font.Font(FONT,30)  
     display_surf = pygame.display.get_surface()
     line_height = pygame.font.Font.get_linesize(f)
 
-    for index, line in enumerate(info_list):
+    for index, line in enumerate(multistring_list):
         debug_surf = f.render(str(line), ANTIALIAS, FONT_COLOR)
-        position = (20, TOPLEFT[1] + index * line_height)
-        debug_rect = debug_surf.get_rect(topleft = position)
+        position = (x, y + index*line_height)
+        debug_rect = debug_surf.get_rect(topleft=position)
         pygame.draw.rect(display_surf, BG_COLOR, debug_rect)
         display_surf.blit(debug_surf, debug_rect)
