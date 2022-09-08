be3f8805167f212fbfdf36e83349f485fa26cbec
ericsnoddy <ericsnoddy@gmail.com>
2022-09-08 18:52:10 -0400

    separated file path data from settings

---
+++ obj/data.py
@@ -0,0 +1,34 @@
+from os.path import join
+
+BG_DEFAULT = join('assets', 'bg', 'space.png')
+
+FONT_TYPE_REG = join('assets', 'fonts', 'Xolonium-Regular.otf')
+FONT_TYPE_BOLD = join('assets', 'fonts', 'Xolonium-Bold.otf')
+
+BUTTONS = {
+    # buttons
+    'stop': join('assets', 'buttons', 'stop.png'),
+    'stop_': join('assets', 'buttons', 'stopping.png'),
+    'prev': join('assets', 'buttons', 'prev.png'),
+    'prev_': join('assets', 'buttons', 'preving.png'),
+    'next': join('assets', 'buttons', 'next.png'),
+    'next_': join('assets', 'buttons', 'nexting.png'),
+    'power': join('assets', 'buttons', 'power.png'),
+    'power_': join('assets', 'buttons', 'powering.png'),
+    'menu': join('assets', 'buttons', 'menu.png'),
+    'menu_': join('assets', 'buttons', 'menued.png'),
+    'mute': join('assets', 'buttons', 'mute.png'),
+    'mute_': join('assets', 'buttons', 'muted.png'),
+    'pause': join('assets', 'buttons', 'pause.png'),
+    'pause_': join('assets', 'buttons', 'paused.png'),
+    'play': join('assets', 'buttons', 'play.png'),
+    'play_': join('assets', 'buttons', 'playing.png'),
+    'rew': join('assets', 'buttons', 'rew.png'),
+    'rew_': join('assets', 'buttons', 'rewing.png'),
+    'ff': join('assets', 'buttons', 'ff.png'),
+    'ff_': join('assets', 'buttons', 'ffing.png'),
+    'voldown': join('assets', 'buttons', 'voldown.png'),
+    'voldown_': join('assets', 'buttons', 'voldowning.png'),
+    'volup': join('assets', 'buttons', 'volup.png'),    
+    'volup_': join('assets', 'buttons', 'voluping.png')
+}
--- obj/settings.py
+++ obj/settings.py
@@ -1,51 +1,33 @@
-from os.path import join
-
 WIDTH = 400
 HEIGHT = 600
-PAD = 32
-BPAD = 8 # pad between buttons (row & col)
-BSIZE = 42 # button size
 FPS = 60
 
-BG_DEFAULT = join('assets', 'bg', 'space.png')
-CAPTION = 'Music Player'
-FONT_TYPE_REG = join('assets', 'fonts', 'Xolonium-Regular.otf')
-FONT_TYPE_BOLD = join('assets', 'fonts', 'Xolonium-Bold.otf')
-FONT_COLOR = '#FFD700'
-BAR_BORDER_COLOR = '#A6A8AC'
+# spacing
+PAD = 32 # edge padding
+BPAD = 8 # pad between buttons (row & col)
+BSIZE = 42 # button size
 
-# volume and seek increment in seconds
+# spacing setup
+ROWPAD = (WIDTH - BSIZE*7 - BPAD*6) // 2
+ROWSLOT = BSIZE + BPAD
+ROW1_Y = HEIGHT - BPAD*3 - BSIZE*3  
+ROW2_Y = HEIGHT - BPAD*2 - BSIZE*2
+ROW3_Y = HEIGHT - BPAD - BSIZE
+
+# UI - program is designed to fit 28 rows of 12px height text + 1 row of 13px height text
+UI_HEIGHT = 349
+LIST_FONTSIZE_REG = 10
+LIST_FONTSIZE_BOLD = 11
+
+# other aesthetics
+FONT_COLOR = '#FFD700'
+BAR_COLOR = FONT_COLOR
+BAR_BORDER_COLOR = '#A6A8AC'
+LIST_BORDER_COLOR = BAR_BORDER_COLOR
+
+# initial volume (out of 100) and seek increment in seconds
 VOL_START = 75
-VOL_MAX = 100
-# VOL_INCR = 3
-SEEK_INCR = 10
+SEEK_INCR = 10  # seconds
+
 # True: 1st load is random, False: 1st load is index 0
 OPEN_RANDOM_MODE = True
-
-BUTTONS = {
-    # buttons
-    'stop': join('assets', 'buttons', 'stop.png'),
-    'stop_': join('assets', 'buttons', 'stopping.png'),
-    'prev': join('assets', 'buttons', 'prev.png'),
-    'prev_': join('assets', 'buttons', 'preving.png'),
-    'next': join('assets', 'buttons', 'next.png'),
-    'next_': join('assets', 'buttons', 'nexting.png'),
-    'power': join('assets', 'buttons', 'power.png'),
-    'power_': join('assets', 'buttons', 'powering.png'),
-    'menu': join('assets', 'buttons', 'menu.png'),
-    'menu_': join('assets', 'buttons', 'menued.png'),
-    'mute': join('assets', 'buttons', 'mute.png'),
-    'mute_': join('assets', 'buttons', 'muted.png'),
-    'pause': join('assets', 'buttons', 'pause.png'),
-    'pause_': join('assets', 'buttons', 'paused.png'),
-    'play': join('assets', 'buttons', 'play.png'),
-    'play_': join('assets', 'buttons', 'playing.png'),
-    'rew': join('assets', 'buttons', 'rew.png'),
-    'rew_': join('assets', 'buttons', 'rewing.png'),
-    'ff': join('assets', 'buttons', 'ff.png'),
-    'ff_': join('assets', 'buttons', 'ffing.png'),
-    'voldown': join('assets', 'buttons', 'voldown.png'),
-    'voldown_': join('assets', 'buttons', 'voldowning.png'),
-    'volup': join('assets', 'buttons', 'volup.png'),    
-    'volup_': join('assets', 'buttons', 'voluping.png')
-}
